9d745361b97e4eec9e851a430fc7460b758fed16
Behruz <marsithb2022@gmail.com>
2024-12-17 18:56:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let mars = {
+    name : prompt("Ismingizni kiriting"),
+    id : prompt("Space idingizni kiriting"),
+    password : prompt("Space passwordingizni kiriting")
+}
+
+console.log(mars);
